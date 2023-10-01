f169c22541a29bc6763b255a549d17469c3b4750
jgao2 <jgao222@uw.edu>
2023-09-30 22:52:44 -0700

    working extractor on single files that are already gifs

---
+++ src/gifparser.rs
@@ -0,0 +1,286 @@
+/// A parser for GIF format, but for my use case just gets the starting indices and lengths of blocks
+/// See https://www.w3.org/Graphics/GIF/spec-gif89a.txt
+
+#[derive(Debug)]
+pub struct GifParseError {
+    pub message: &'static str,
+}
+impl GifParseError {
+    fn new(message: &'static str) -> Self {
+        Self { message }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum GifBlockType {
+    Header,
+    LogicalScreenDescriptor,
+    ColorTable,
+    Extension,
+    ImageDescriptor,
+    ImageData,
+    Trailer,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Block {
+    pub index: u32,
+    pub size: u32,
+    pub block_type: GifBlockType,
+}
+
+#[derive(Debug, Default)]
+pub struct GifParser<'a> {
+    cur_index: u32,
+    bytes: &'a [u8],
+    blocks: Vec<Block>,
+}
+
+impl<'a> GifParser<'a> {
+    pub fn new() -> Self {
+        Self {
+            ..Default::default()
+        }
+    }
+
+    pub fn parse_gif_from_bytes(&mut self, bytes: &'a [u8]) -> Result<Vec<Block>, GifParseError> {
+        self.bytes = bytes;
+        self.blocks = vec![];
+        self.cur_index = 0;
+
+        // header and logical screen descriptor are always present
+        self.parse_header()?;
+        self.parse_logical_screen_descriptor()?;
+
+        // check for global color table
+        // on most modern systems casting to usize shouldn't lose data
+        let logical_packed_byte = bytes[(self.blocks.last().unwrap().index + 4) as usize];
+        if (logical_packed_byte & 0x80) != 0 {
+            // global color table flag is set to true
+            let global_color_table_size = logical_packed_byte & 0x07; // number of entries
+
+            // spec says 3 x 2^(Size of Global Color Table+1), since we are right shifting
+            // value 2 already it already has implicit 2^1 so subtract 1 from that
+            let global_color_table_bytes = 3 * (2 << (global_color_table_size));
+            println!(
+                "Global color table size {} bytes and cur index is {}",
+                global_color_table_bytes, self.cur_index
+            );
+
+            // parse global color table
+            self.parse_color_table(global_color_table_bytes)?;
+        }
+
+        // loop and parse extension blocks and image blocks
+        // only three characters may appear now, ! for graphic control extension blocks,
+        // , for image descriptor and subsequent image data
+        // and ; the trailing byte
+        loop {
+            println!("current index {}", self.cur_index);
+            match bytes[self.cur_index as usize] {
+                b'!' => {
+                    // 0x21
+                    self.parse_extension()?;
+                }
+                b',' => {
+                    // 0x2C
+                    // parse descriptor
+                    self.parse_image_descriptor()?;
+
+                    // if the local color table bit was set, parse local color table
+                    let image_descriptor_packed_byte =
+                        self.bytes[(self.blocks.last().unwrap().index + 9) as usize];
+                    if image_descriptor_packed_byte & 0x80 != 0 {
+                        // local color table exists
+                        let local_color_table_size = logical_packed_byte & 0x06; // number of entries
+
+                        // see above when getting size of global color table
+                        let local_color_table_bytes = 3 * (2 << (local_color_table_size));
+
+                        self.parse_color_table(local_color_table_bytes)?;
+                    }
+
+                    // parse image data
+                    self.parse_image_data()?;
+                }
+                b';' => {
+                    // 0x3B
+                    // we know this will definitely be the end because if we encounter
+                    // blocks or graphic control extension blocks we will jump past them
+                    self.parse_trailer()?;
+                    break;
+                }
+                byte => panic!(
+                    "encountered GIF byte {:x?} that isn't a block starter! blocks so far {:?}",
+                    byte, self.blocks
+                ),
+            };
+        }
+
+        Ok(self.blocks.clone())
+    }
+
+    /// Parse the GIF header from the current block
+    fn parse_header(&mut self) -> Result<(), GifParseError> {
+        const GIF_HEADER: [u8; 4] = *b"GIF8";
+        const HEADER_LEN: u32 = 6;
+        if !self.bytes.starts_with(&GIF_HEADER) {
+            return Err(GifParseError::new("Invalid GIF header bytes"));
+        }
+        self.blocks.push(Block {
+            index: self.cur_index,
+            size: HEADER_LEN,
+            block_type: GifBlockType::Header,
+        });
+        self.cur_index += HEADER_LEN; // length of full gif header
+        Ok(())
+    }
+
+    fn parse_logical_screen_descriptor(&mut self) -> Result<(), GifParseError> {
+        // not going to validate anything here, just take the 7 bytes
+        const LOGICAL_SCREEN_DESCRIPTOR_LEN: u32 = 7;
+        self.blocks.push(Block {
+            index: self.cur_index,
+            size: LOGICAL_SCREEN_DESCRIPTOR_LEN,
+            block_type: GifBlockType::LogicalScreenDescriptor,
+        });
+        self.cur_index += LOGICAL_SCREEN_DESCRIPTOR_LEN;
+        Ok(())
+    }
+
+    /// Since color tables can be variable length specified by the preceding
+    /// blocks, pass the length in here of how many bytes to include as the table
+    fn parse_color_table(&mut self, length: u32) -> Result<(), GifParseError> {
+        // again, not doing any validation
+        self.blocks.push(Block {
+            index: self.cur_index,
+            size: length,
+            block_type: GifBlockType::ColorTable,
+        });
+        self.cur_index += length;
+        Ok(())
+    }
+
+    /// There are many different types of graphic control extension, which may have their
+    /// data set in different ways.
+    fn parse_extension(&mut self) -> Result<(), GifParseError> {
+        if self.bytes[self.cur_index as usize] != 0x21 {
+            return Err(GifParseError::new("Invalid extension block declaration"));
+        }
+        // Graphic control, plain text, and application extensions all have the format:
+        //  extension introducer byte -> label byte -> block size
+        // but plain text and application also have sub blocks of arbitrary length
+        // Comment extensions only have sub blocks, extension introducer -> label byte -> sub blocks of comment data
+        // they all have a zero byte block terminator that we should check though
+        let idx = self.cur_index as usize;
+        let total_size = match self.bytes[idx + 1] {
+            0xF9 => {
+                // graphic control extension
+                // only block size
+                let block_size = self.bytes[idx + 2];
+                // three extra bytes since introducer, label, and size aren't included in the size
+                3 + block_size as u32
+            }
+            0xFE => {
+                // comment data extension
+                // only sub blocks
+                let data_size = self.length_of_sub_blocks(idx + 2);
+                // extension introducer byte and label byte
+                2 + data_size
+            }
+            0x01 | 0xFF => {
+                // plain text and application extension
+                // block size for metadata and sub blocks with their own size
+                let metadata_size = self.bytes[idx + 2];
+                let sub_blocks_start = idx + 3 + metadata_size as usize;
+                let sub_blocks_size = self.length_of_sub_blocks(sub_blocks_start);
+                3 + metadata_size as u32 + sub_blocks_size
+            }
+            _ => return Err(GifParseError::new("Invalid extension block type")),
+        };
+        self.blocks.push(Block {
+            index: self.cur_index,
+            size: total_size,
+            block_type: GifBlockType::Extension,
+        });
+        self.cur_index += total_size;
+
+        if self.bytes[self.cur_index as usize] != 0x0 {
+            Err(GifParseError::new("Invalid extension block terminator"))
+        } else {
+            self.cur_index += 1; // advance past block terminator
+            Ok(())
+        }
+    }
+
+    fn parse_image_descriptor(&mut self) -> Result<(), GifParseError> {
+        const IMAGE_DESCRIPTOR_LEN: u32 = 10;
+        if self.bytes[self.cur_index as usize] != 0x2C {
+            return Err(GifParseError::new(
+                "Invalid image descriptor block declaration",
+            ));
+        }
+        self.blocks.push(Block {
+            index: self.cur_index,
+            size: IMAGE_DESCRIPTOR_LEN,
+            block_type: GifBlockType::ImageDescriptor,
+        });
+        self.cur_index += IMAGE_DESCRIPTOR_LEN;
+        Ok(())
+    }
+
+    fn parse_image_data(&mut self) -> Result<(), GifParseError> {
+        const LZW_MIN_CODE_SIZE_LEN: u32 = 1;
+        let data_len = self.length_of_sub_blocks((self.cur_index + LZW_MIN_CODE_SIZE_LEN) as usize);
+        // if self.bytes[(self.cur_index + data_len) as usize] != 0x0 {
+        //     return Err(GifParseError::new("Invalid image data"));
+        // }
+        self.blocks.push(Block {
+            index: self.cur_index,
+            size: data_len + LZW_MIN_CODE_SIZE_LEN,
+            block_type: GifBlockType::ImageData,
+        });
+        self.cur_index += data_len + LZW_MIN_CODE_SIZE_LEN;
+
+        if self.bytes[self.cur_index as usize] != 0x0 {
+            Err(GifParseError::new(
+                "Invalid termination of image data blocks",
+            ))
+        } else {
+            self.cur_index += 1; // advance past block terminator
+            Ok(())
+        }
+    }
+
+    /// Get the length of a list of sub-blocks starting at index, not including the terminating 0 byte
+    /// This might yield ridiculous results if the index isn't actually the start of
+    /// a list of blocks!
+    fn length_of_sub_blocks(&mut self, index: usize) -> u32 {
+        // data sub block format is just length, data bytes, null (0)
+        // if the next byte after data is not null, then it is another sub-block in the list
+        let mut num_bytes: u32 = 0;
+
+        let mut block_len = self.bytes[index];
+        while block_len != 0 {
+            num_bytes += 1 + block_len as u32; // add one to include the length byte as well
+            block_len = self.bytes[index + (num_bytes as usize)];
+        }
+
+        num_bytes
+    }
+
+    fn parse_trailer(&mut self) -> Result<(), GifParseError> {
+        if self.bytes[self.cur_index as usize] != 0x3B {
+            // the GIF trailer byte
+            Err(GifParseError::new("Invalid GIF trailer byte!"))
+        } else {
+            self.blocks.push(Block {
+                index: self.cur_index,
+                size: 1,
+                block_type: GifBlockType::Trailer,
+            });
+            self.cur_index += 1;
+            Ok(())
+        }
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,183 @@
+use image::ImageFormat;
+use std::{fs, io::Write};
+
+use crate::gifparser::GifParser;
+
+pub mod gifparser;
+
+#[derive(Debug)]
+pub enum Error {
+    GifError(&'static str),
+}
+pub fn extract_images(filename: &String, output_folder: &String) -> Result<(), Error> {
+    // try to open file and read bytes from it
+    let bytes = fs::read(filename).expect("file path argument should be valid");
+
+    // at every byte, ask the image library to try guessing if there is an image
+    // here (based on magic bytes)
+    // if so, remember what format that is, ask it to make it into an image, then
+    // save it in that format
+    // TODO isn't this unnecessary work since the bytes are already in the proper format?
+    //      it would be more efficient to just find where the image begins and ends
+    //      and splat those direcly to disk without needing to hold the image in memory first...
+    // but it should still work? and it should get all image types supported by the
+    // image crate?
+    let mut num_images = 0;
+    let mut i = 0;
+    while i < bytes.len() {
+        if let Ok(format) = image::guess_format(&bytes[i..]) {
+            if format == ImageFormat::Gif {
+                // gifs don't appear to be properly handled (image crate only takes the first frame)
+                // so instead if we see the gif start header we will manually search for the
+                // end bytes ourselves and write out the bytes and frames manually
+                // TODO This didn't work because there were 0x3B's and 0x003B's showing
+                //      up before EOF and ignoring them would have been missing out on much of the data
+                //      how do modern decoders handle seeing the trailer byte? shouldn't the trailer byte
+                //      signal the definite end of the GIF file???
+                //      Turns out the GIF Format goes like this
+                //      If you see the GIF header, then you are guaranteed some other metadata like
+                //       color tables, logical screen descriptor. Then there is optionally some extensions blocks. Then
+                //       there are image data blocks, which importantly have a byte field specifying how
+                //       many bytes are in the block. So even if the trailing byte is seen, if it is within
+                //       a block we know it is data!
+                //       Unfortunately, this makes it hard to know where the GIF ends without knowing
+                //       where the last block is, meaning we need to parse block data out of the GIF,
+                //       so we need to know about the GIF structure and write a parser/state machine for it...
+                // assert!(bytes[i..i + GIF_HEADER.len()] == GIF_HEADER);
+                // let start_idx = i;
+                // i += GIF_HEADER.len(); // no need to check the gif header/magic bytes for the trailer
+                // while bytes[i] != 0x3B || bytes[i - 1] != 0x00 {
+                //     i += 1;
+                // }
+                // fs::write(
+                //     format!("{}/{}.gif", output_folder, num_images),
+                //     &bytes[start_idx..i + 1],
+                // )
+                // .expect("manually outputting gif bytes should write without error");
+                // println!("GIF ended after {} bytes", i - start_idx);
+                // num_images += 1;
+
+                // This approach with re-encoding and de-encoding doesn't preserve
+                // stuff like if the gif repeats, and also balloons the filesize
+                // for some reason
+                // let manual_output_file =
+                //     fs::File::create(format!("{}/{}.gif", output_folder, num_images))
+                //         .expect("should be able to create a file");
+
+                // let decoder = GifDecoder::new(&bytes[i..]).expect("gif was properly formatted");
+                // let mut encoder = GifEncoder::new(manual_output_file);
+
+                // encoder
+                //     .try_encode_frames(decoder.into_frames())
+                //     .expect("directly encoding decoded gif frames should be fine");
+
+                // num_images += 1;
+
+                // instead, let's just try giving the gif every byte until another
+                // image file's magic numbers are found or EOF?
+
+                // let's try jumping over blocks, don't read the data inside
+                // ugh, we also need to jump over extension blocks, optional global color table,
+                // and optional local color tables...
+                // and all that involves linked lists of sub-blocks...
+                // let start_idx = i;
+                // while bytes[i] != 0x3B {
+                //     if bytes[i] == 0x2C {
+                //         // 0x2C is `,` comma, the block starting delimiter
+                //         // the length of the block is 11 bytes away from the block start
+                //         i += bytes[i + 11] as usize; // u8 should fit in usize...
+                //     } else {
+                //         i += 1;
+                //     }
+                // }
+                // Let's just give whole block until next file header then
+                // will lead to bloated image files, but all the original bytes will
+                // be there...
+                // that doesn't work if some block just happens to have data that looks like a header.
+
+                // TODO handle when GIF not parsed properly (still want to continue w/ rest of file)
+                find_and_write_gif(&bytes, i, format!("{}/{}.gif", output_folder, num_images))?;
+                num_images += 1;
+            } else if let Ok(img) = image::load_from_memory_with_format(&bytes[i..], format) {
+                img.save_with_format(
+                    format!(
+                        "{}/{}.{}",
+                        output_folder,
+                        num_images,
+                        format.extensions_str()[0]
+                    ),
+                    format,
+                )
+                .expect("image should save properly");
+                num_images += 1;
+            }
+        }
+        i += 1;
+    }
+    println!("Extracted {} images", num_images);
+
+    // let mut png_header_offsets = vec![];
+
+    // let mut jpeg_header_offsets = vec![];
+
+    // // match bytes against PNG header and see how many times it shows up
+    // for i in 0..(bytes.len() - PNG_HEADER.len()) {
+    //     if bytes[i..i + PNG_HEADER.len()] == PNG_HEADER {
+    //         png_header_offsets.push(i);
+    //     }
+    //     // TODO technically the byte sequence of each file type's header could
+    //     // appear as legitimate data in another file, so to do this properly
+    //     // we would need to keep track of whether we are already in a file or
+    //     // not
+    //     if bytes[i..i + JPEG_HEADER.len()] == JPEG_HEADER {
+    //         jpeg_header_offsets.push(i);
+    //     }
+    // }
+
+    // println!("Saw the PNG header {} times", png_header_offsets.len());
+    // println!("Saw the JPEG header {} times", jpeg_header_offsets.len());
+
+    // println!("Attempting to read out and save all pngs");
+    // for (i, offset) in png_header_offsets.iter().enumerate() {
+    //     let img = image::load_from_memory(&bytes[*offset..]) // this gives all the way to end of file, which isn't great
+    //         .expect("should find a correct image from the header");
+
+    //     img.save(format!("{}/{}.png", output_folder, i))
+    //         .expect("should save image successfully");
+    // }
+
+    // println!("Attempting to read out and save all jpegs");
+    // let mut non_jpegs = 0;
+    // for (i, offset) in jpeg_header_offsets.iter().enumerate() {
+    //     if let Ok(img) = image::load_from_memory(&bytes[*offset..]) {
+    //         img.save(format!("{}/{}.jpg", output_folder, i))
+    //             .expect("should save image successfully");
+    //     } else {
+    //         non_jpegs += 1;
+    //     }
+    // }
+    // println!(
+    //     "Only {} were proper jpegs!",
+    //     jpeg_header_offsets.len() - non_jpegs
+    // );
+    Ok(())
+}
+
+fn find_and_write_gif(bytes: &[u8], index: usize, filename: String) -> Result<(), Error> {
+    let start_idx = index;
+    let mut parser = GifParser::new();
+    let blocks = parser
+        .parse_gif_from_bytes(bytes)
+        .map_err(|err| Error::GifError(err.message))?;
+    let end_idx = (blocks.last().unwrap().index + blocks.last().unwrap().size) as usize;
+
+    let mut manual_output_file =
+        fs::File::create(filename).expect("should be able to create a file");
+
+    manual_output_file
+        .write_all(&bytes[start_idx..end_idx]) // +1 to include the trailer byte
+        .expect("should be able to write segement to disk");
+
+    println!("GIF ended after {} bytes", end_idx - start_idx);
+    Ok(())
+}
