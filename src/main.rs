ba8889f044f3d2d8c89d41655002bbda9ce123f2
jgao2 <jgao222@uw.edu>
2023-10-02 08:32:05 -0700

    feat: add a more helpful message when bad arguments passed in

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,12 @@ use extractor::{extract_images, Error};
 use std::env::args;
 
 fn main() -> Result<(), Error> {
-    // const PNG_HEADER: [u8; 8] = [137, 80, 78, 71, 13, 10, 26, 10];
-    // const JPEG_HEADER: [u8; 2] = [0xFF, 0xD8];
-    // this technically isn't completely right since there is a more specific version as well on the end
-    // const GIF_HEADER: [u8; 4] = *b"GIF8";
-
     let args: Vec<String> = args().collect();
 
-    assert!(args.len() == 3);
+    if args.len() != 3 {
+        eprintln!("Error: Bad Args\nUsage: ./extractor.exe [FILE] [TARGET_DIRECTORY]");
+    }
+
     let filename = &args[1];
     let mut output_folder = args[2].clone();
     if !output_folder.ends_with('/') {
